beab11cd7f7eab426c7ca2ccdf2783c9f8e17f91
MiloBarral <milobb@gmail.com>
2014-10-09 16:52:35 +0200

    Update testservos.ino

--- testservos.ino
+++ testservos.ino
@@ -1,1 +1,27 @@
-// sketch para probar servos
+//  AnalogReadSerial + ServoKnob para ver como se mueven los SG90 trucados.
+//  Resultados:
+//    <---    <-    .    .    ->    --->
+//     179   130   105  102   85     0 
+//     179   120   103  101   79     0
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+ 
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin 
+ 
+void setup() 
+{ 
+    Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+} 
+ 
+void loop() 
+{ 
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  val = map(val, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
+  myservo.write(val);                  // sets the servo position according to the scaled value 
+  delay(15);   
+Serial.println(val);  // waits for the servo to get there 
+} 
