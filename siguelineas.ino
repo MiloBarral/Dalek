ab41ef02cfbebdb44f0d00ef4fa2f99cbda91ff5
MiloBarral <milobb@gmail.com>
2014-10-09 16:50:58 +0200

    Update siguelineas.ino

--- siguelineas.ino
+++ siguelineas.ino
@@ -41,7 +41,7 @@ void loop()
   d=p-p_old;
   p_old=p;
   
-  if ((p*i)<0) i=0;  	// corrige el overshooting - integral windup
+  if ((p*i)<0) i=0;  		// corrige el overshooting - integral windup
 
   u=kp*p+ki*i+kd*d;             // Suma PID
   
