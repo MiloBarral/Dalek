93277ef35949d6254372d70bbb6f2c2eeefee305
MiloBarral <milobb@gmail.com>
2014-10-16 10:15:09 +0200

    Update testservos.ino

--- testservos.ino
+++ testservos.ino
@@ -22,6 +22,6 @@ void loop()
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
   val = map(val, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
   myservo.write(val);                  // sets the servo position according to the scaled value 
-  delay(15);   
-Serial.println(val);  // waits for the servo to get there 
+  delay(15);                           // waits for the servo to get there 
+Serial.println(val);                   // Sends the value in order to view it in the SerialMonitor
 } 
